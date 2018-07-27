e9a0874688da052b68ca039036e1b3e88aac9f84
raviteja-6 <41757213+raviteja-6@users.noreply.github.com>
2018-07-27 15:08:28 +0530

    Update alphabate.py

--- alphabate.py
+++ alphabate.py
@@ -1,8 +1,8 @@
 value= raw_input()
 if (value >= 'a' and value <='z') or (value >='A' and value <='Z'):
 	if value in ['a','e','i','o','u','A','E','I','O','U']:
-		print('vowel')
+		print('Vowel')
 	else:
-		print('consonant')
+		print('Consonant')
 else:
 	print("invalid")
