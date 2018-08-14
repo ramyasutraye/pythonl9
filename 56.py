c703e1d24f20095a6ea45ca7fcf77f7482571e23
raviteja-6 <41757213+raviteja-6@users.noreply.github.com>
2018-08-14 15:20:41 +0530

    Create 56.py

---
+++ 56.py
@@ -0,0 +1,14 @@
+n1=[str(x) for x in input()]
+c=0
+for i in n1:
+    if i.isalpha():
+        c=0
+    elif i.isdigit():
+        c=0
+    else:
+        c+=1
+if c==0:
+    print("Yes")
+else:
+    print("No")
+        
